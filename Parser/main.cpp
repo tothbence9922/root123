0ea34d4dbdcd06045b07a0b1f4ca6abad1c894e8
Gyorgy Luptak <lgyuri97@gmail.com>
2021-11-10 21:20:28 +0100

    removed print

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -3,17 +3,14 @@
 int main(int argc, char* argv[]) {
 	CAFF* caff;
 	if (argc == 1) {
-		std::cout << "1 arg" << std::endl;
 		caff = new CAFF();
 		caff->parse();
 	}
 	else if (argc == 2) {
-		std::cout << "2 arg" << std::endl;
 		caff = new CAFF(argv[1]);
 		caff->parse();
 	}
 	else if (argc == 3) {
-		std::cout << "3 arg" << std::endl;
 		caff = new CAFF(argv[1], argv[2]);
 		caff->parse();
 	}
