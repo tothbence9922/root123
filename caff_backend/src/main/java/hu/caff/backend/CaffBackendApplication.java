4ebddc11a596cde5ec0230f72e589dea5cbebf43
Tóth Vince <vincetot97@gmail.com>
2021-12-01 13:10:37 +0100

    Changing setup to MySQl db.

--- caff_backend/src/main/java/hu/caff/backend/CaffBackendApplication.java
+++ caff_backend/src/main/java/hu/caff/backend/CaffBackendApplication.java
@@ -1,14 +1,14 @@
 package hu.caff.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@Configuration
-@EnableJpaRepositories(basePackages = {"hu.caff.backend"})
 public class CaffBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CaffBackendApplication.class, args);
